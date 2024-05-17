023f67fdd6a90cffd92a0b566e6cc0fe65def9de
mavlone <mavlonweb@gmai.com>
2024-05-17 18:15:39 +0500

    message

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+
+const App = () => {
+  
+  return (
+    <div>
+      <Header title="hello" />
+      <Header title="ikkinchiis" />
+      <Header title="uchinchisi" />
+      <Header />
+      <Intro />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="intro">
+        <div className="container">
+          <h1>hello bu intro</h1>
+          <h2>byebye</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
